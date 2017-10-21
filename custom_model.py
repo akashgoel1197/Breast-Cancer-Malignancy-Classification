7de7551c29433f1f0c1b84e231da411465f3f481
Ayoosh Kathuria <kathuria.ayoosh@gmail.com>
2017-10-21 17:10:42 +0530

    Initial Commit

---
+++ custom_model.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 13:01:07 2017
+
+@author: ayooshmac
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt 
+matplotlib.style.use('ggplot')
+from torchnet import meter
+
+import pickle as pkl 
+
+import torch 
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+from torch import nn
+from torchvision import transforms 
+
+class custom_model(object):
+    def __init__(self, model, loss_fn):
+        self.model = model
+        self.original_loss_fn = loss_fn
+        self.loss_fn = loss_fn
+        self.losses = []
+        self.losses_test = []
+        self.accus = []
+        self.accus_train = []
+        self.vals = []
+    
+    def train(self, trainloader, testloader, validloader, optimizer, epochs, plot = False):
+        self.losses = []
+        self.losses_test = []
+        self.accus = []
+        self.accus_train = []
+        patience = 8
+        j = 0
+        prev_valid_score = 0
+        best_valid = 0
+        
+        for epoch in range(epochs):
+            for i, data in enumerate(trainloader):
+                x, y = data
+                x,y = Variable(x), Variable(y)
+                optimizer.zero_grad()
+                
+                outputs = self.model(x)
+
+                loss = self.loss_fn(outputs, y)
+                loss.backward()
+                optimizer.step()
+                
+            #implementation of early stopping 
+            
+            
+            #logging metrics for plotting, 
+            self.losses.append(self.get_loss(trainloader))
+            self.losses_test.append(self.get_loss(testloader))
+            self.accus.append(self.metrics_val(testloader)[0])
+            self.accus_train.append(self.metrics_val(trainloader)[0])
+            
+            curr_valid_score = (self.metrics_val(validloader)[0])
+            
+            if curr_valid_score > best_valid:
+                best_valid  =  curr_valid_score
+            if curr_valid_score <= prev_valid_score:
+                j = j + 1
+            else:
+                j = 0
+#            if (j == patience):
+#                #print("Epochs trained: ", epoch)
+#                #print(self.metrics_val(testloader)[0])
+#                optimizer = optim.SGD(model.parameters(), lr/5.0, weight_decay = wd)
+#                j = 0
+#                if (clock>2):
+#                    break
+#                    print("The validation accuracy was", curr_valid_score)
+#                clock += 1
+            if (j == patience):
+                #print("Epochs trained: ", epoch)
+                #print(self.metrics_val(testloader)[0])
+                break
+                
+            prev_valid_score = curr_valid_score
+        print("The validation accuracy was", curr_valid_score)
+
+#            print("The validation accuracy was", curr_valid_score)
+
+    def get_loss(self, dataloader):
+        loss = 0
+        for data in dataloader:
+            x,y = data
+            x,y = Variable(x), Variable(y)
+            outputs = self.model(x) 
+            loss += self.loss_fn(outputs, y).data[0]
+        return loss/float(dataloader.dataset.shape[0])
+    
+    def metrics(self, testloader, accuracy = True, auc = False, conf_matrix = False):
+        am = meter.AUCMeter()
+        cm = meter.ConfusionMeter(2)
+        correct = 0
+        total = 0
+        for data in testloader:   
+           
+            x,y = data
+            y_ = self.model(Variable(x))
+            _, predicted = torch.max(y_.data, 1)
+          
+            cm.add(y_.data, y)
+            
+            am.add(y_.data[:,1].clone(),y)
+            total += y.size(0)
+            correct += (predicted == y).sum()
+        print (correct, total)
+        if accuracy:
+            print("Accuracy for the model is", round(correct/float(total)*100, 4), correct, "/", total)
+        
+        if auc:
+            print("Area under ROC curve for the given model is", round(am.value()[0],4))
+        
+        if conf_matrix:
+            print ("Confusion Matrix for the given model is\n", cm.value())
+    
+    
+    def metrics_val(self, testloader):
+        am = meter.AUCMeter()
+        cm = meter.ConfusionMeter(2)
+        correct = 0
+        total = 0
+        for data in testloader:   
+            x,y = data
+            y_ = self.model(Variable(x))
+            _, predicted = torch.max(y_.data, 1)
+            cm.add(y_.data, y)
+            am.add(y_.data[:,1].clone(),y)
+            total += y.size(0)
+            correct += (predicted == y).sum()
+        
+        return round(correct/float(total)*100, 4), round(am.value()[0],4), cm.value()    
+        
+            
+            
+    def get_logs(self):
+        return self.losses, \
+            self.losses_test, \
+            self.accus, \
+            self.accus_train
+            
+    def plot(self, logs):
+        losses, losses_test, accus, accus_train = logs
+        steps = range(1, len(losses)+1)
+        
+        plt.plot(steps, losses, color = 'r', label = 'Training Loss')
+        plt.plot(steps, losses_test, color = 'b', label = 'Test Loss')
+        plt.xlabel("Epochs")
+        plt.legend(['Training Loss', 'Test Loss'])
+#            
+        plt.figure(2)
+        plt.xlabel("Epochs")
+        plt.plot(steps, accus, color = 'b', label = 'Train Accuracy')
+        plt.plot(steps, accus_train, color = 'r', label = 'trTrain Accuracy')
+        plt.legend(['Test accuracy', 'Training Accuracy'])
+        
+    
+    def set_loss(self, loss_fn):
+        self.loss_fn = loss_fn
+    
+    def  reset_loss(self):
+        self.loss_fn = self.original_loss_fn
+    
---
+++ loaders.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 08:45:23 2017
+
+@author: ayooshmac
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt 
+matplotlib.style.use('ggplot')
+from torchnet import meter
+
+import pickle as pkl 
+
+import torch 
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+from torch import nn
+from torchvision import transforms 
+
+class WBCDataset(Dataset):
+    """
+    Loads the Wisconsin Breast Cancer dataset from a csv file into a PyTorch Dataset 
+    Object which can then be passed to an iterable DataLoader object.
+    """
+    def __init__(self, df, transform = None):
+        """
+        Initialises the WBCDataset Instance from 
+        
+        df: dataframe containing the data
+        Transform: default None, PyTorch transform object to be apply on the data
+        
+        """
+        
+        self.df = df
+        self.shape = df.shape
+        self.transform = transform
+    
+    def __len__(self):
+        return self.df.shape[0]
+    
+    def __getitem__(self, idx):
+        x = self.df.drop(["class"], axis = 1)
+        y = self.df["class"]
+        x = (torch.from_numpy(x.iloc[idx].astype(np.float32).values))
+        y = int(y.iloc[idx])
+   
+        sample = x,y
+        
+        return sample
+
+
+def get_dataloaders(datasets, transform = None, batch_size = 5, shuffle = True):
+    """
+    Takes a 3-tuple constaining Pandas Dataframes containing Training, Test and 
+    Validation datasets respectively and returns a corresponding 3-tuple containing
+    PyTorch DataLoader objects
+    
+    datasets: 3-tuple constaining Pandas Dataframes containing Training, Test and 
+              Validation datasets respectively
+    
+    transform: List of PyTorch Transforms.Compose objects to be applied to
+               the data
+    
+    Batch size: Batchsize to be used while training
+    
+    Shuffle: Shuffles the datasets if True
+    
+    Returns: Corresponding 3-tuple containingPyTorch DataLoader objects
+    """
+    
+    train, test, valid = datasets
+    trainset = WBCDataset(train, transform)
+    testset = WBCDataset(test, transform)
+    validset = WBCDataset(valid, transform)
+    
+    
+    trainloader = DataLoader(trainset, shuffle= True, batch_size=batch_size, num_workers=4)
+    testloader = DataLoader(testset, shuffle= True, batch_size=batch_size, num_workers=4)
+    validloader = DataLoader(validset, shuffle= True, batch_size=batch_size, num_workers=4)
+    
+    return trainloader, testloader, validloader    
+    
+class loaders(object):
+    """
+    Loads the data from a CSV file. Capable of paritioned data either in form 
+    of Pandas datframes, or PyTorch Dataloader object
+
+    """
+    def __init__(self, csv, preprocess = False):
+        """
+        Initialise the instance with a CSV file containing data, with an option
+        to supply a pre-processing function for it
+        
+        csv:        CSV file containing the data
+        preprocess: A function that takes a pandas dataframe as an input and 
+                    outputs a pandas dataframe. Use it to pre-process your data
+                    such as dataframe with pre-processed data is returned.
+                    
+        Returns: None
+        """
+        self.csv = csv
+        self.df = pd.read_csv("data.csv")
+        self.preprocess = preprocess
+        if preprocess:
+            self.df = self.preprocess(self.df)
+    
+    def get_partitions(self, partitions):
+        """
+        Partitions data into training, test and validation data in form of pandas
+        dataframes
+        
+        partitions: A list containing three values between 0 and 1.0, where the
+                    first, the second and the third element denotes the fraction 
+                    of the data to be partitioned into Training, test and valid-
+                    ation set respectively. The sum of the elements must equal 
+                    1.0
+        
+        Returns: A 3-tuple containing pandas Dataframes containing the training, 
+                 test and validation sets respectively
+    
+        """
+        num_train = int(partitions[0]*self.df.shape[0])
+        num_test = int(partitions[1]*self.df.shape[0])
+        num_valid = int(partitions[2]*self.df.shape[0])
+        df_copy = self.df.copy()
+        
+        df_copy = df_copy.sample(frac = 1).reset_index(drop=True) #shuffle the data
+
+        train_set = self.df[:num_train]
+        test_set = self.df[num_train:num_train + num_test]
+        valid_set = self.df[num_train + num_test:]
+        
+        return train_set, test_set, valid_set
+    
+    def get_loaders(self, partitions, transform = None, batch_size = 5, shuffle = True):
+        """
+        Partitions data assosciated with the loader object into training, test 
+        and validation data in form of PyTorch DataLoader objects. It can also 
+        return DataLoader objects for a 3-tuple containing training, test and 
+        validation data
+        
+        partitions: A list containing three values between 0 and 1.0, where the
+                    first, the second and the third element denotes the fraction 
+                    of the data to be partitioned into Training, test and valid-
+                    ation set respectively. The sum of the elements must equal 
+                    1.0
+        
+        
+        transform: List of PyTorch Transforms.Compose objects to be applied to
+                   the data
+        
+        batch_size: Batch size to be used while training
+        
+        shuffle:  Shuffle the dataset
+                   
+        
+        Returns: A 3-tuple containing PyTorch DataLoader objects containing the
+                 training, test and validation sets respectively
+    
+        """
+        
+        
+        datasets = self.get_partitions(partitions)
+        return get_dataloaders(datasets, transform = None, batch_size = None, shuffle = True)
+        
+    
+    
+    
+    
+        
+        
+        
+    
